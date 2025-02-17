9c55d8fabf2117c2c1e4d254396a6e47e408455c
ashleyxnicolej <ashleyjackson439@gmail.com>
2025-02-17 03:19:58 -0500

    Add untracked files and merge main into final-implement

---
+++ Develop/cypress/component/Quiz.cy.js
@@ -0,0 +1,6 @@
+// filepath: cypress/component/Quiz.cy.jsx
+describe('Quiz Component', () => {
+    it('should render the quiz component', () => {
+      // Add your test code here
+    });
+  });
---
+++ Develop/cypress/e2e/quiz.cy.js
@@ -0,0 +1,6 @@
+// filepath: cypress/e2e/quiz.cy.js
+describe('Tech Quiz', () => {
+    it('should start the quiz and display questions', () => {
+      // Add your test code here
+    });
+  });
